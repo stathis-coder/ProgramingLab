3c404568a077716fe345b9729924fbc71fbecd9e
repl.it user <replituser@example.com>
2020-12-04 16:20:44 +0000

    Terzo commit: versione coretta di es3oggettoCSVfile.py

--- es3oggettoCSVfile.py
+++ es3oggettoCSVfile.py
@@ -1,3 +1,4 @@
+#CARO MORICHETTI PER QUESTO ESERCIZIO NON SERVE PIU CHE LO CONTROLLI, SONO RIUSCITO A RISOLVERE IL PROBLEMA
 class CSVFile():
     #definisco il metodo init che istanzia l'oggetto
     def __init__(self,name):
@@ -6,9 +7,9 @@ class CSVFile():
     #creo il metodo get_data che che torni i dati del file CSV
     def get_data(self):
         #inizializzo una lista nella quale salvare i valori convertiti
-        converted_valures = []
+        converted_values = []
         #apro il mio file
-        my_file = open(self.name, 'r')
+        my_file = open(self.name,'r')
         #per ogni riga del mio file:
         for line in my_file:
             #separo la stringa in piu stringhe usando "," come separatore
@@ -21,15 +22,15 @@ class CSVFile():
                 #converto le vendite da stringhe a valori
                 #e le appendo alla lista 
                 converted_values.append(float(values))
-            #chiudo il mio file
-            my_file.close()
-            #retorno la lista con i valori numerici
-            return converted_values
+        #chiudo il mio file
+        my_file.close()
+        #retorno la lista con i valori numerici
+        return converted_values
 
 
 #test
-file = CSVFile("shampoo_sales")
-data = file.getdata()
+file = CSVFile("shampoo_sales.csv")
+data = file.get_data()
 print(data)
 
     
